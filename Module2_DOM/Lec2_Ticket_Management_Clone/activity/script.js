d8bc92073cdaae5cf336849e9fb9f6269f24d777
Ansh <anshaggarwal9999@gmail.com>
2021-05-02 13:13:28 +0530

    Ticket Management System completed

--- Module2_DOM/Lec2_Ticket_Management_Clone/activity/script.js
+++ Module2_DOM/Lec2_Ticket_Management_Clone/activity/script.js
@@ -11,12 +11,16 @@ let selectedFilter = "black";
 let allFilters = document.querySelectorAll(".ticket-filters div");
 let ticketContainer = document.querySelector(".tickets-container");
 let openModalBtn = document.querySelector(".open-modal");
+let closeModalBtn = document.querySelector('.close-modal');
 
 function loadTickets() {
+  ticketContainer.innerHTML = "";
+
   if(localStorage.getItem('allTickets')) {
     let allTickets = JSON.parse(localStorage.getItem('allTickets'));
 
     for(let i = 0; i < allTickets.length; i++) {
+      //object destructuring
       let ticketObject = allTickets[i];
       // console.log(ticketObject);
       // create a div and add class ticket to it
@@ -25,17 +29,76 @@ function loadTickets() {
       ticketDiv.classList.add("ticket");
       // set the html of the ticket wala div !!
       ticketDiv.innerHTML = ` <div class="ticket-filter ${ticketObject.ticketFilter}"></div>
+      <div class="ticket-info">
       <div class="ticket-id">#${ticketObject.ticketId}</div>
+      <div class="ticket-delete">
+        <i class="fas fa-trash" id = ${ticketObject.ticketId}></i>
+      </div>
+      </div>
       <div class="ticket-content">${ticketObject.ticketContent}</div>`;
 
       // append the ticket on the UI !!!!
       ticketContainer.append(ticketDiv);
+
+      ticketDiv.querySelector('.ticket-delete i').addEventListener("click", handleTicketDelete);
+      ticketDiv.querySelector('.ticket-filter').addEventListener("click", toggleTicketFilter);
     }
   }
 }
 loadTickets();
 
 openModalBtn.addEventListener("click", handleOpenModal);
+closeModalBtn.addEventListener("click", handleCloseModal);
+
+function toggleTicketFilter(e) {
+  console.log(e);
+  let currentFilter = e.target.classList[1];
+  let filters = ["red" , "blue" , "green" , "black"];
+
+  let idx = filters.indexOf(currentFilter);
+  idx++;
+  idx = idx % filters.length;
+
+  let currentTicket = e.target;
+  currentTicket.classList.remove(currentFilter);
+  currentTicket.classList.add(filters[idx]);
+
+  let allTickets = JSON.parse(localStorage.getItem("allTickets"));
+  let id = currentTicket.nextElementSibling.children[0].textContent.split("#")[1];
+  console.log(id);
+
+  for(let i=0 ; i<allTickets.length ; i++){
+    if(allTickets[i].ticketId == id){
+      allTickets[i].ticketFilter = filters[idx];
+      break;
+    }
+  }
+
+  localStorage.setItem("allTickets" , JSON.stringify(allTickets));
+}
+
+function handleTicketDelete(e) {
+  let confirmation = window.prompt("You Want to delete it or not");
+  if(confirmation == "Yes" || confirmation == "yes") {
+    let ticketToBeDeleted = e.target.id;
+
+    let allTickets = JSON.parse(localStorage.getItem("allTickets"));
+
+    let filteredTickets = allTickets.filter(function(ticketObject) {
+      return ticketObject.ticketId != ticketToBeDeleted;
+    });
+
+    localStorage.setItem("allTickets", JSON.stringify(filteredTickets));
+
+    loadTickets();
+  }
+}
+
+function handleCloseModal(e) {
+  if(document.querySelector(".modal")){
+    document.querySelector(".modal").remove();
+  }
+}
 
 function handleOpenModal(e) {
   let modal = document.querySelector(".modal");
@@ -94,7 +157,7 @@ function chooseModalFilter(e) {
   // set selected filter as the now choose filter !!!
   selectedFilter = selectedModalFilter;
   // remove active filter class
-  document.querySelector(".active-filter").classList.remove("active-filter");
+  document.querySelector(".modal-filter.active-filter").classList.remove("active-filter");
   // add active filter class on now selected filter 
   e.target.classList.add("active-filter");
 }
@@ -110,11 +173,19 @@ function addTicket(e) {
     ticketDiv.classList.add("ticket");
     // set the html of the ticket wala div !!
     ticketDiv.innerHTML = ` <div class="ticket-filter ${selectedFilter}"></div>
-    <div class="ticket-id">#${ticketId}</div>
+    <div class="ticket-info">
+      <div class="ticket-id">#${ticketId}</div>
+      <div class="ticket-delete" tId = ${ticketId}>
+        <i class="fas fa-trash" id=${ticketId}></i>
+      </div>
+    </div>
     <div class="ticket-content">${modalText}</div>`;
 
     // append the ticket on the UI !!!!
     ticketContainer.append(ticketDiv);
+
+    ticketDiv.querySelector('.ticket-delete i').addEventListener("click", handleTicketDelete);
+    ticketDiv.querySelector('.ticket-filter').addEventListener("click", toggleTicketFilter);
 
     // remove the modal from the ui !!!
     e.target.parentNode.remove();
@@ -166,7 +237,56 @@ for (let i = 0; i < allFilters.length; i++) {
 }
 
 function chooseFilter(e) {
-  let filter = e.target.classList[1];
-  let filterCode = filterCodes[filter];
-  ticketContainer.style.background = filterCode;
+  if(e.target.classList.contains('active-filter')) {
+    e.target.classList.remove('active-filter');
+    loadTickets();
+    return;
+  }
+
+  if(document.querySelector('.filter.active-filter')) {
+    document.querySelector('.filter.active-filter').classList.remove('active-filter');
+  }
+  // let filter = e.target.classList[1];
+  e.target.classList.add('active-filter');
+  let ticketFilter = e.target.classList[1];
+
+  loadSelectedTickets(ticketFilter);
+  // let filterCode = filterCodes[filter];
+  // ticketContainer.style.background = filterCode;
+}
+
+function loadSelectedTickets(ticketFilter) {
+  if(localStorage.getItem("allTickets")){
+    let allTickets = JSON.parse(localStorage.getItem("allTickets"));
+    
+    let filteredTickets = allTickets.filter( function(filterObject){
+      return filterObject.ticketFilter == ticketFilter;
+    });
+
+    // empty tickets container
+    ticketContainer.innerHTML = "";
+
+    for(let i = 0; i < filteredTickets.length; i++) {
+      let {ticketId, ticketFilter, ticketContent} = filteredTickets[i];
+
+      // create a div and add class ticket to it
+      let ticketDiv = document.createElement("div");
+      //get an unique ID
+      ticketDiv.classList.add("ticket");
+      // set the html of the ticket wala div !!
+      ticketDiv.innerHTML = `<div class="ticket-filter ${ticketFilter}"></div>
+      <div class="ticket-info">
+      <div class="ticket-id">#${ticketId}</div>
+      <div class="ticket-delete" tId = ${ticketId}>
+      <i class="fas fa-trash" id=${ticketId}></i>
+      </div>
+      </div>
+      <div class="ticket-content">${ticketContent}</div>`;
+
+      // append the ticket on the UI !!!!
+      ticketContainer.append(ticketDiv);
+      
+      ticketDiv.querySelector('.ticket-delete i').addEventListener("click", handleTicketDelete);
+    }
+  }
 }
