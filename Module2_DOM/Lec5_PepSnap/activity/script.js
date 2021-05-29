8484b97141ff566012d1112ff8d069063087cc7e
Ansh <anshaggarwal9999@gmail.com>
2021-05-29 17:04:13 +0530

    Pep Scan Indexed DB

--- Module2_DOM/Lec5_PepSnap/activity/script.js
+++ Module2_DOM/Lec5_PepSnap/activity/script.js
@@ -75,46 +75,60 @@ let curZoom = 1;
     });
 })();
 
-function saveVideoToFs(){
+
+function saveVideoToFs() {
     console.log("Saving Video");
     // file object in recordedData
-    let videoUrl = URL.createObjectURL(recordedData);
+    let videoUrl = URL.createObjectURL(recordedData); // convert Blob object into Blob Url
     // console.log(videoUrl);
-
-    let aTag = document.createElement("a");
-    aTag.download = "video.mp3";
-    aTag.href = videoUrl;
-
-    aTag.click(); 
-    aTag.remove();
-}
-
-function capturePhotos() {
-    console.log("Saving Photo ....");
-
+  
+    let iv = setInterval( function(){
+      if(db){
+        saveMedia("Video" , videoUrl);
+        clearInterval(iv);
+      }
+    }  , 100 );
+    // let aTag = document.createElement("a");
+    // aTag.download = "video.mp4";
+    // aTag.href = videoUrl;
+  
+    // console.log(aTag);
+    // aTag.click(); // download start for video
+  }
+  
+  function capturePhotos() {
     photoButton.querySelector("div").classList.add("capture-animate");
-
-    setTimeout(function() {
-        photoButton.querySelector("div").classList.remove("capture-animate");
-    }, 1000);
-
+    // async
+    setTimeout(function(){
+      photoButton.querySelector("div").classList.remove("capture-animate");
+    } , 1000);
+  
     let canvas = document.createElement("canvas");
     canvas.height = videoPlayer.videoHeight;
     canvas.width = videoPlayer.videoWidth;
     let ctx = canvas.getContext("2d");
-
+  
     // canvas is scaled according to currZoom
     if(curZoom != 1){
-        ctx.translate(canvas.width/2 , canvas.height/2);
-        ctx.scale(curZoom , curZoom);
-        ctx.translate(-canvas.width/2 , -canvas.height/2)
+      ctx.translate(canvas.width/2 , canvas.height/2);
+      ctx.scale(curZoom , curZoom);
+      ctx.translate(-canvas.width/2 , -canvas.height/2)
     }
-
+  
     ctx.drawImage(videoPlayer, 0, 0);
-
-    let aTag = document.createElement("a");
-    let imageUrl = canvas.toDataURL("image/jpg");
-    aTag.download = "photo.jpg";
-    aTag.href = imageUrl;
-    aTag.click();
-}
+    let imageUrl = canvas.toDataURL("image/jpg"); //canvas object => file url String
+  
+    let iv = setInterval( function(){
+      if(db){
+        saveMedia("image" , imageUrl);
+        clearInterval(iv);
+      }
+    }  , 100 );
+    
+  
+    // skip the downloading part !!!
+    // let aTag = document.createElement("a");
+    // aTag.download = "photo.jpg";
+    // aTag.href = imageUrl;
+    // aTag.click();
+  }
